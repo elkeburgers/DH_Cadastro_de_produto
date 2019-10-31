b1e400050b662577a54942e8b6f437853af8b0fd
Elke Burgers <elke.burgers@hotmail.com>
2019-10-30 21:36:03 -0300

    Criacao de html e php da pagina de produto - falta trazer informacao de nome do cadastroProduto.php

--- produto.php
+++ produto.php
@@ -1,16 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel='stylesheet' href='/css/style.css'>
     <title>Produto</title>
 </head>
+
 <body>
-    
-    
+
+    <?php include_once('variaveis.php'); ?>
+    <section class='container bg-light p-4 mt-5'>
+        <button class='mb-3'><a href='cadastroProduto.php'>&larr;Voltar para a lista de produtos</a></button>
+        <div class='row'>
+            <?php 
+            foreach($produtos as $produto){
+                if($_GET['nome'] == $produto['nome']){
+            ?>
+            <div class='col-5'>
+                <img src="<?php echo $produto['img']; ?>" class='card-img' alt="Imagem do produto">
+            </div>
+            <div class='col-7'>
+                <div class="card-body">
+
+                    <h1><?php echo $produto['nome'] ?></h1>
+
+                    <h6>Categoria</h6>
+                    <p><?php echo $produto['categ'] ?></p>
+
+                    <h6>Descrição</h6>
+                    <p><?php echo $produto['desc'] ?></p>
+
+                    <div class='d-flex justify-content-between'>
+                        <div>
+                            <p>Quantidade em estoque</p>
+                            <p><?php echo $produto['qtde'] ?></p>
+                        </div>
+                        <div>
+                            <p>Preço</p>
+                            <p><?php echo  $produto['preco'] ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php    
+                }
+            }
+            ?>
+        </div>
+    </section>
 
 </body>
+
 </html>
